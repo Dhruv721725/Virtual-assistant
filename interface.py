39e8cb0318b43fe2da2d4946c8863dc2f97d1598
Dhruv721725 <dhruv721725@gmail.com>
2024-02-14 18:38:54 +0530

    Jimmy_VA

---
+++ interface.py
@@ -0,0 +1,16 @@
+# from tkinter import *
+# from PIL import ImageTk,Image
+# root=Tk()
+# root.title('Jimmy')
+# root.iconbitmap('')
+# root.configure(background='orange')
+# root.geometry('400x200')
+# root.maxsize(400,200)
+# root.minsize(400,200)
+
+# img=Image.open()
+# emoji=Label(root,image=img)
+# emoji.pack()
+
+
+# root.mainloop()
